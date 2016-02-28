8795e41daa2e7f00459f9b13dc0f869340e44e49
Benjamin Downs <benjamin.downs@gmail.com>
2016-02-28 14:19:29 +0100

    redirects working now

--- server/PicoRedirects.js
+++ server/PicoRedirects.js
@@ -4,51 +4,34 @@ var mongo = require('mongodb').MongoClient;
 module.exports = function (req, res) {
 
     var url = req.params.url.toString()
-    // var result
-    if (!url) {
-        return null;
+
+    function connect() {
+        mongo.connect(db, function (err, db) {
+            if (err) throw err
+            getURL(db, function (doc) {
+                res.redirect(doc.original)
+                db.close()
+
+            })
+        }); // end of mongo connect
     }
 
-    mongo.connect(db, function (err, db) {
-        if (err) throw err
-        getURL(db, function () {
-            db.close()
-            
-        })
-    }); // end of mongo connect
-  
     function getURL(db, callback) {
+        console.log(req.url)
         db.collection('shortened')
             .findOne({ 'picoURL': url }
             // ,{ original: 1 } use projection later
                 , function (err, doc) {
                     if (err) throw err
-                    console.log('doc')
-                    res.redirect(doc['original'])
-                    callback()
+                    callback(doc)
                 });
     }
-        
-    // cursor.each(function (err, doc) {
-    //     if (doc != null) {
-    //         console.log('hello')
-    //         console.dir(doc)
-    //         res.redirect(doc.original)
-    //         callback();
-    //     } else {
-    //         console.log('sorry, no url found')
-    //         res.send(url + ' sorry, no url found')
-    //         callback();
-    //     }
-    // });
-    // end of getURL function
+
+    if (!url || url == 'favicon.ico') {
+        return null;
+    } else {
+        connect()
+    }
     
-    
-    
-    
-    // query database for url as key and redirect to value
-    
-    
-    // res.redirect(result)
     // res.send('hello from the bottom line of the redirects module');
 };
